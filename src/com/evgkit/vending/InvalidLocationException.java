5c6932553f6a0ca18f0ad8025eff3ca4d1a879b1
evgkit <kitaev.ea@gmail.com>
2017-01-23 13:52:18 +0300

    Initial commit. Cowboy style

---
+++ src/com/evgkit/vending/InvalidLocationException.java
@@ -0,0 +1,7 @@
+package com.evgkit.vending;
+
+public class InvalidLocationException extends Exception {
+    public InvalidLocationException(String s) {
+        super(s);
+    }
+}
---
+++ src/com/evgkit/vending/Notifier.java
@@ -0,0 +1,5 @@
+package com.evgkit.vending;
+
+public interface Notifier {
+    void onSale(Item item);
+}
